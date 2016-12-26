c12bf7e0eeb790e75147c0c6a798701b818e1739
doubleUTF <dlau373@gmail.com>
2016-12-26 15:26:31 -0800

    Initialized note app for node course

---
+++ notes.js
@@ -0,0 +1,24 @@
+console.log('Starting notes.js');
+
+var addNote=(title,body)=>{
+  console.log('Adding note: ', title,body);
+};
+
+var getAll=()=>{
+  console.log('Getting all notes');
+}
+
+var getNote=(title)=>{
+  console.log('Getting',title);
+}
+
+var deleteNote=(title)=>{
+  console.log('Deleting', title);
+}
+
+module.exports={
+  addNote,
+  getAll,
+  getNote,
+  deleteNote
+}
